6c9cceb135c97f7465340cb791362d9e328227a5
Shreyas GN <136763743+Shreyas-GN@users.noreply.github.com>
2024-10-24 12:15:02 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,9 @@
+anime = 'dragonball'
+if anime == 'one piece':
+  print("Monkey D luffy is the king of pirates")
+elif anime == 'naruto':
+  print("Naruto is ninja king")
+elif anime == 'dragonball':
+  print("Saiyan king")
+else:
+  print("Anime not in list")
